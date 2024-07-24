f757a5024126d791c69d0cb5449fd9b64a359250
Ramin Rezaei <rare@boozt.com>
2024-07-24 22:41:23 +0200

    feat: add google client id

--- src/shared/constants/environment.ts
+++ src/shared/constants/environment.ts
@@ -3,5 +3,6 @@ export const ENV = import.meta.env || {};
 export const ENV_DATA = {
   NAME: ENV.VITE_APP_NAME || 'FLIRTIO',
   VERSION: ENV.VITE_APP_VERSION || '0.0.1',
+  GOOGLE_CLIENT_ID: ENV.VITE_GOOGLE_CLIENT_ID || '',
   BASE_URL: ENV.VITE_API_BASE_URL || 'http://localhost:8000'
 };
